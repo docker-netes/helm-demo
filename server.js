504f227f77aa4f9f2a2e5d78f2ce68ae5fc25ff3
docker-netes <44562731+docker-netes@users.noreply.github.com>
2022-06-28 12:21:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 85;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Hello Helm World\n');
 }).listen(port);
